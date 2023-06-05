6d5ef4b8d8d02112713777ee2854d3438fdbecf8
gauravkadam935 <120292352+gauravkadam935@users.noreply.github.com>
2023-06-05 13:19:17 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,26 @@
-import React from 'react'
+import React,{useState} from 'react'
 import '../styles/App.css';
 import {useSelector,useDispatch} from "react-redux";
 import {changeColor,changeStyle } from '../actions/index.js';
 
 const App = () => {
 
-const dispatch =useDispatch();
-//code here 
-  if(changeColor){
-    changeStyle("green");
-  }else{
-    changeStyle("red");
+  const [color,setColor]=useState("red");
+  const [fontFamily,setfontFamily]=useState("Arial");
+  const handleClick=(e)=>{
+    const id= e.target.id;
+    if(id=="colorBtn"){
+     setColor((prev)=>prev=='red'?"green":"red");
+      return;
+    }
+      
+  setfontFamily((prev)=>prev=='Arial'?"Georgia":"Arial");
   }
-
   return (
     <div id="main">
-      <h1 id='text' style={{color: colorState, fontFamily: styleState}}>Newton School</h1>
-      <button id='colorBtn' onClick={()=>dispatch(changeColor(colorState))}>Change Color</button>
-      <button id='styleBtn' onClick={()=>dispatch(changeStyle(styleState))}>Change Style</button>
+      <h1 id='text' style={{color, fontFamily}}>Newton School</h1>
+      <button id='colorBtn' onClick={handleClick}>Change Color</button>
+      <button id='styleBtn' onClick={handleClick}>Change Style</button>
 
     </div>
   )
